14ff77972427497c6c61e39daf6dd272c5c3c095
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-09-20 19:36:29 -0400

    Added robohash monster images to card-list

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,9 +6,16 @@ class CardList extends Component {
 
         return (
             <div>
-                {monsters.map((monster) => (
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))}
+                {monsters.map((monster) => {
+                    const { name, email, id } = monster;
+                    return (
+                        <div className='card-container' key={monster.id}>
+                            <img alt={`monster ${monster.name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    )
+                })}
             </div>
         );
     }
