60c28026803e47bf52d2bf71a19da504cc9339a6
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2022-09-20 14:56:49 -0400

    Added search functionality

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => 
+        this.setState(
+          () => {return {monsters: users}}
+        )
+      );
+  }
+  
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {return {searchField};});
+  }
+
+  render() {
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
+          return <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>;
+        })}
+        <CardList monster={filteredMonsters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
